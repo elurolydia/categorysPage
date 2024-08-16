9fc8b2e37df1312a38139c4f53655402b8eb3c58
Lydia Eluro <elurolydia@gmail.com>
2024-08-16 15:49:25 +0100

    added to the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,13 @@ import instagram from '../assets/icons/instagram.png';
 import X from '../assets/icons/X.png';
 import call from '../assets/icons/call.png';
 import logo from '../assets/images/logo.png';
+import search from '../assets/icons/search.png';
 
 const Navbar = () => {
   return (
     <div>
       <div className='topNav'>
-        <div>
+        <div className='handles'>
           <img src={facebook} alt='' />
           <img src={instagram} alt='' />
           <img src={X} alt='' />
@@ -19,12 +20,18 @@ const Navbar = () => {
           Discover top-quality seeds, tools, and supplies for a bountiful
           harvest.
         </p>
-        <div>
+        <div className='call'>
           <img src={call} alt='' />
           <span>+1 (850) 344 0 66</span>
         </div>
       </div>
-      <img src={logo} alt='' />
+      <div className='middleNav'>
+        <img src={logo} alt='' />
+        <div className='searchField'>
+          <img src={search} alt='' />
+          <input type='text' placeholder='Search for your product here ' />
+        </div>
+      </div>
     </div>
   );
 };
